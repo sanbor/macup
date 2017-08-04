16131fbef86233077aa38824b668bca15a084f55
Santiago Borrazas <santiago@borrazas.org>
2017-08-03 23:47:26 -0300

    Initial commit

---
+++ dot.zshrc
@@ -0,0 +1,41 @@
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/Users/user/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+PROMPT='[%n@%m %~]$ '
+
+# Make time look like bash instead of the zsh version
+disable -r time       # disable shell reserved word
+alias time='time -p ' # -p for POSIX output
+# or  TIMEFMT=$'\nreal\t%*Es\nuser\t%*Us\nsys\t%*Ss'
+
+#fix annoying zsh word jump behaviour
+bindkey '^[^[[C' forward-word # alt right arrow to jump word
+bindkey '^[^[[D' backward-word # alt left arrow to jump backwards word
+autoload -U select-word-style
+select-word-style bash
+
+export ANT_HOME=/usr/local/opt/ant
+export MAVEN_HOME=/usr/local/opt/maven
+export GRADLE_HOME=/usr/local/opt/gradle
+export ANDROID_HOME=/usr/local/opt/android-sdk
+export ANDROID_NDK_HOME=/usr/local/opt/android-ndk
+
+export PATH=$ANT_HOME/bin:$PATH
+export PATH=$MAVEN_HOME/bin:$PATH
+export PATH=$GRADLE_HOME/bin:$PATH
+export PATH=$ANDROID_HOME/tools:$PATH
+export PATH=$ANDROID_HOME/platform-tools:$PATH
+export PATH=$ANDROID_HOME/build-tools/19.1.0:$PATH
+
+# alias
+alias ll='gls --color -lh'
+alias la='ll -art'
---
+++ up.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+# Basic macOS bootstrap
+
+# Exit when some command fails (more info http://www.davidpashley.com/articles/writing-robust-shell-scripts/#id2382181)
+set -e
+
+echo "Please enter your full name to be used in git config:"
+read name
+
+echo "Please enter email to generate ssh key and to be set in git config:"
+read email
+
+# Set hostname to host to be more generic
+sudo scutil –-set HostName host
+
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Disable Google analytics
+brew analytics off
+
+# Install Google Chrome, Firefox, iTerm2, java, etc.
+brew cask install google-chrome firefox iterm2 vlc atom virtualbox vagrant \
+  docker vagrant-manager sublime-text santa djview mullvad xquartz gimp \
+  inkscape anki java eclipse-jee intellij-idea libreoffice boxer mysqlworkbench \
+  torbrowser insomnia veracrypt osxfuse
+
+# maybe useful cyberduck sweet-home3d
+
+# Install git, syncthing, pass, etc.
+# gotchas: gpg-agent is required to avoid issues with pass
+brew install git syncthing pass gpg gpg-agent mc ranger mplayer ffmpeg youtube-dl zsh zsh-completions \
+  autojump tmux emacs pandoc node mtr aria2 python ipython plantuml ext4fuse
+
+# optionals
+# brew install yarn nginx
+# brew cask install visual-studio-code google-drive
+
+# Install GNU utilities (they are more updated than the ones shipped with macOS)
+# Guide to avoid appending the g in some commands https://www.topbug.net/blog/2013/04/14/install-and-use-gnu-command-line-tools-in-mac-os-x/
+brew install coreutils binutils diffutils ed findutils gawk gnu-indent gnu-sed \
+  gnu-tar gnu-which gnutls grep gzip screen watch wdiff wget bash gdb gpatch \
+  m4 make nano file-formula git less openssh rsync svn unzip vim
+
+# Atom packages
+apm install plantuml-viewer hydrogen
+
+# Npm packages
+npm install --global ijavascript gulp-cli create-react-app lite-server
+
+# Python packages
+pip3 install --upgrade pip
+pip3 install jupyter jupyter_contrib_nbextensions pandas
+
+# Setup zsh (more info https://wiki.archlinux.org/index.php/zsh)
+cp ~/.zshrc ~/.zshrc.bak || true # Try to backup just in case
+cp ./dot.zshrc ~/.zshrc
+echo "[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh">>~/.zshrc
+echo "export PATH=\"/usr/local/bin:/usr/local/sbin:~/bin:$PATH\"">>~/.zshrc
+sudo echo "$(which zsh)" >> /etc/shells
+chsh -s $(which zsh)
+
+# Create keys for the machine
+mkdir ~/.ssh || true
+ssh-keygen -t rsa -b 4096 -C "$email"
+eval "$(ssh-agent -s)"
+mv ~/.ssh/.ssh_config ~/.ssh/.ssh_config.back || true # Try to backup just in case
+cp ~/dot.ssh_config ~/.ssh/config
+ssh-add -K ~/.ssh/id_rsa
+
+# Setup git
+cp ~/.gitignore ~/.gitignore.bak || true # Try to backup just in case
+cp ./dot.gitignore ~/.gitignore
+git config --global user.name "$name"
+git config --global user.email "$email"
+git config --global core.editor vim
+git config --list
+
+# Disable Chrome mouse gestures
+defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScrolls -bool false
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+
+# Setup android development, from https://gist.github.com/patrickhammond/4ddbe49a67e5eb1b9c03
+brew install ant maven gradle
+brew cask install brew cask install android-studio
